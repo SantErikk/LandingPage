297ebbdf1004afd8791da353c2c991854af7603c
Erik Sanost <santoserik11@hotmail.com>
2025-09-24 15:31:33 -0300

    Restaurando src para estado anterior

--- src/App.js
+++ src/App.js
@@ -1,31 +1,13 @@
-import './App.css';
-import Modal from 'react-modal'
-import BeneficiosSection from './components/BeneficiosSection';
-import EconomizarSection from './components/EconomizarSection';
-import FaturarSection from './components/FaturarSection';
-import Header from './components/Header';
-import SobreSection from './components/SobreSection';
-import Container from './components/Container';
-import SoluctionSection from './components/SolucaoSection';
-import DataSecuritySection from './components/DataSecuritySection';
-import ContatoForm from './components/ContatoSection';
-import FooterSection from './components/Footersection';
+export default App;
+
+
+import Container from "./components/Container";
+import Home from "./pages/Home";
+
 function App() {
   return (
     <Container>
-      <Header />
-      <main>
-        <SobreSection />
-        <FaturarSection />
-        <EconomizarSection />
-        <BeneficiosSection />
-        <SoluctionSection />
-        <DataSecuritySection />
-        <ContatoForm />    
-        <FooterSection />   
-        <Modal />
-      </main>
+      <Home />
     </Container>
   );
 }
-export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,30 @@
+import './App.css';
+import Modal from 'react-modal'
+import BeneficiosSection from './components/BeneficiosSection';
+import EconomizarSection from './components/EconomizarSection';
+import FaturarSection from './components/FaturarSection';
+import Header from './components/Header';
+import SobreSection from './components/SobreSection';
+import Container from './components/Container';
+import SoluctionSection from './components/SolucaoSection';
+import DataSecuritySection from './components/DataSecuritySection';
+import ContatoForm from './components/ContatoSection';
+import FooterSection from './components/Footersection';
+function App() {
+  return (
+    <Container>
+      <Header />
+      <main>
+        <SobreSection />
+        <FaturarSection />
+        <EconomizarSection />
+        <BeneficiosSection />
+        <SoluctionSection />
+        <DataSecuritySection />
+        <ContatoForm />    
+        <FooterSection />   
+        <Modal />
+      </main>
+    </Container>
+  );
+}
